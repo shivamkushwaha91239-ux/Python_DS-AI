27390e50903434d097f66b96eabc4fb190aa31c1
Shivam Kumar <shivamkushwaha91239@gmail.com>
2026-06-25 10:28:20 +0530

    second day

---
+++ day2/print.py
@@ -0,0 +1,24 @@
+#method 1
+print("Hello world")
+
+#method 2
+message = "Hello world"
+print(message)
+
+#method 3
+text1 = "Hello"
+text2 = "world"
+print(text1 + " " + text2)
+
+#method 4
+message = "Hello world"
+print(f"{message}")
+
+#method 5
+import sys
+sys.stdout.write("Hello world\n")
+
+#method 6
+def greet():
+    return "Hello world"
+
